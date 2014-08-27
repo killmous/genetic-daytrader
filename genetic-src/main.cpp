00fb8f2d7d1bdf99fc78a503683c08a7857dfd69
killmous <wcaldwell.email@gmail.com>
2014-08-27 12:14:01 -0500

    Added some stuff (I think)  Changes to be committed: 	modified:   genetic-src/main.cpp

--- genetic-src/main.cpp
+++ genetic-src/main.cpp
@@ -1,29 +1,35 @@
 #include <cstdio>
+#include <string>
 #include "sqlite3.h"
 #include "ga.h"
+
+using namespace std;
 
 bool bitAt(int offset, Chromosome chromo) {
     return (chromo >> (offset - 1)) & 0b1;
 }
 
-int iterations = 10000000;
+int iterations = 1000;
 int popSize = 300;
-int entropy = 64;
+int entropy = 8;
 
 double fitness(Chromosome chromo) {
     return 1.0/(abs(chromo - 69) + 1);
 }
 
 int main(int argc, char **argv) {
+    if(argc < 2) {
+        printf("Incorrect usage\n");
+        exit(1);
+    }
+    string str(argv[1]);
     sqlite3 *db;
     char *zErrMsg = nullptr;
     int rc;
 
-    rc = sqlite3_open("test.db", &db);
+    rc = sqlite3_open(str.c_str(), &db);
 
-    if(rc) {
-        printf("Error opening test.db\n");
-    }
+    sqlite3_close(db);
 
     Population pop(popSize, entropy, 0.001, 0.1, fitness);
     pop.run(iterations);
